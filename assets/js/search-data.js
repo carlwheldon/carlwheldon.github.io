ddc899758405b46e60867e66117059b8759afc76
carlwheldon <64424334+carlwheldon@users.noreply.github.com>
2025-08-21 14:31:09 +0000

    Deploying to gh-pages from @ carlwheldon/carlwheldon.github.io@c10ad3c811a358604788e69fbff218f96f00897d 🚀

--- assets/js/search-data.js
+++ assets/js/search-data.js
@@ -32,7 +32,7 @@ ninja.data = [{
           },
         },{id: "nav-cv",
           title: "cv",
-          description: "CV page underconstruction.",
+          description: "",
           section: "Navigation",
           handler: () => {
             window.location.href = "/cv/";
